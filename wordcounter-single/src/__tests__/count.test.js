5c0e27e74734813a3ac8685cd56d5ed15e513658
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-01-31 01:32:59 +0000

    adding tests!

---
+++ wordcounter-single/src/__tests__/count.test.js
@@ -0,0 +1,18 @@
+import countWords from '../countWords'
+import {describe, expect, it } from '@jest/globals'
+
+
+describe('the counting Function', () => {
+    it('counts the number of words', () => {
+        expect(countWords("One Two three")).toBe(3)
+    })
+
+    it('counts an empty string', () => {
+        expect(countWords('')).toBe(0)
+    })
+    
+    it('counts other string', () => {
+        expect(countWords('do bee do bee dooo')).toBe(5)
+    })
+    
+}) 
