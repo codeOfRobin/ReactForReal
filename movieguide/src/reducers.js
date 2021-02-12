170764c2a03f49db32974ae1691a60b45fd471db
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-02-12 01:59:44 +0000

    Add reducers

--- movieguide/src/reducers.js
+++ movieguide/src/reducers.js
@@ -13,3 +13,30 @@ export function movies(state = [], action) {
             return state;
     }
 }
+
+export function loading(state = true, action) {
+    switch (action.type) {
+        case MOVIES_LOADED:
+            return false
+        default:
+            return state
+    }
+}
+
+export function filter(state = false, action) {
+    switch (action.type) {
+        case FILTER_CHANGED:
+            return action.filter
+        default:
+            return state
+    }
+}
+
+export function favorites(state = [], action) {
+    switch (action.type) {
+        case FAVORITED:
+            return [...state, action.movieID]
+        default:
+            return state
+    }
+}
